9f79216164538dd3b51c181b51a5b80bb24ec120
naffonly <nafjie02@gmail.com>
2022-12-05 08:37:26 +0700

    add feature route transaction anda detail transaction

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\CollectionController;
+use App\Http\Controllers\DetailTransactionController;
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -21,6 +23,13 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
 Route::get('user',[UserController::class,'index'])->name('users.index');
 Route::get('userRegistration',[UserController::class,'create'])->name('users.create');
 Route::post('userStore',[UserController::class,'store'])->name('userStore');
@@ -34,16 +43,25 @@ Route::post('koleksiStore',[CollectionController::class,'store'])->name('koleksi
 Route::get('koleksiView/{collection}',[CollectionController::class,'show'])->name('koleksiView');
 Route::post('koleksiUpdate',[CollectionController::class,'update'])->name('collecion.update');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('transaksi',[TransactionController::class,'index'])->name('transaksi.index');
+Route::get('transaksiTambah',[TransactionController::class,'create'])->name('transaksi.create');
+Route::get('transaksiStore',[TransactionController::class,'store'])->name('transaksiStore');
+Route::post('transaksiShow',[TransactionController::class,'show'])->name('transaksiShow');
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('detailTransactionKembalikan/{id}',[DetailTransactionController::class,'detailTransactionKembalikan'])->name('dTransactionView');
+Route::post('detailTransactionUpdate',[DetailTransactionController::class,'update'])->name('dTransaction.update');
+
+
+Route::get('getAllTransactions', [UserController::class, 'getAllTransactions'])
+        ->middleware(['auth','verified']);
+Route::get('getAllDetailTransactions/{id}', [CollectionController::class, 'getAllDetailTransactions'])
+        ->middleware(['auth','verified']);
 
 Route::get('getAllUsers', [UserController::class, 'getAllUsers'])
         ->middleware(['auth','verified']);
 Route::get('getAllCollections', [CollectionController::class, 'getAllCollections'])
         ->middleware(['auth','verified']);
 Auth::routes();
+
 
 require __DIR__.'/auth.php';
